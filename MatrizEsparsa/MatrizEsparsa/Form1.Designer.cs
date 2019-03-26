d838f2ce8910a5d8645c7d7423dd1b98adcf6904
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-03-26 11:42:51 -0300

    Criação e design do Formulário do Projeto

--- MatrizEsparsa/MatrizEsparsa/Form1.Designer.cs
+++ MatrizEsparsa/MatrizEsparsa/Form1.Designer.cs
@@ -28,12 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.btnLerMatriz = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnLerInserir = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSomarK = new System.Windows.Forms.Button();
+            this.btnSomarMatrizes = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(188, 85);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.Size = new System.Drawing.Size(387, 322);
+            this.dgvMatriz.TabIndex = 0;
+            // 
+            // btnLerMatriz
+            // 
+            this.btnLerMatriz.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnLerMatriz.Location = new System.Drawing.Point(188, 37);
+            this.btnLerMatriz.Name = "btnLerMatriz";
+            this.btnLerMatriz.Size = new System.Drawing.Size(121, 42);
+            this.btnLerMatriz.TabIndex = 1;
+            this.btnLerMatriz.Text = "Ler Arquivo";
+            this.btnLerMatriz.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(12, 166);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(121, 40);
+            this.btnInserir.TabIndex = 2;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // btnLerInserir
+            // 
+            this.btnLerInserir.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLerInserir.Location = new System.Drawing.Point(188, 413);
+            this.btnLerInserir.Name = "btnLerInserir";
+            this.btnLerInserir.Size = new System.Drawing.Size(121, 46);
+            this.btnLerInserir.TabIndex = 3;
+            this.btnLerInserir.Text = "Ler e Inserir";
+            this.btnLerInserir.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(12, 227);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(121, 40);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(12, 350);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(121, 40);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar Matriz";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(12, 283);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 46);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Remover Elemento";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(450, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Matrizes Esparsas";
+            // 
+            // btnSomarK
+            // 
+            this.btnSomarK.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomarK.Location = new System.Drawing.Point(331, 413);
+            this.btnSomarK.Name = "btnSomarK";
+            this.btnSomarK.Size = new System.Drawing.Size(121, 46);
+            this.btnSomarK.TabIndex = 8;
+            this.btnSomarK.Text = "Somar Constante K";
+            this.btnSomarK.UseVisualStyleBackColor = true;
+            // 
+            // btnSomarMatrizes
+            // 
+            this.btnSomarMatrizes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomarMatrizes.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnSomarMatrizes.Location = new System.Drawing.Point(602, 413);
+            this.btnSomarMatrizes.Name = "btnSomarMatrizes";
+            this.btnSomarMatrizes.Size = new System.Drawing.Size(120, 46);
+            this.btnSomarMatrizes.TabIndex = 9;
+            this.btnSomarMatrizes.Text = "Somar Matrizes";
+            this.btnSomarMatrizes.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(602, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(387, 322);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(331, 465);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 25);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "K";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(869, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 46);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Multiplicar Matrizes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSomarMatrizes);
+            this.Controls.Add(this.btnSomarK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnLerInserir);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnLerMatriz);
+            this.Controls.Add(this.dgvMatriz);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Button btnLerMatriz;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnLerInserir;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSomarK;
+        private System.Windows.Forms.Button btnSomarMatrizes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
