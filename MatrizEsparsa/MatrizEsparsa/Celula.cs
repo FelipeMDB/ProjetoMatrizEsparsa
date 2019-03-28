d333f3d7c223b254d6b905ec679033c019963e9d
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-03-28 10:21:14 -0300

    Classes celula e matriz circular

---
+++ MatrizEsparsa/MatrizEsparsa/Celula.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Celula
+{
+    protected Celula abaixo;
+    protected Celula direita;
+    protected int valor;
+    protected int linha;
+    protected int coluna;
+
+    public Celula(Celula abaixo, Celula direita, int linha, int coluna, int valor)
+    {
+        Abaixo = abaixo;
+        Direita = direita;
+        Coluna = coluna;
+        Linha = linha;
+        Valor = valor;
+    }
+
+    public override string ToString()
+    {
+        return $"({linha}, {coluna}, {valor})";
+    }
+
+    public Celula Abaixo { get => abaixo; set => abaixo = value; }
+    public Celula Direita { get => direita; set => direita = value; }
+    public int Valor { get => valor; set => valor = value; }
+    public int Coluna { get => coluna; set => coluna = value; }
+    public int Linha { get => linha; set => linha = value; }
+}
+
